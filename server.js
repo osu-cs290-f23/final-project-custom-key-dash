f800d157acacb6b0f2f4e17367a0f61fea331335
DyRayz <135666138+DyRayz@users.noreply.github.com>
2023-12-13 00:08:16 -0800

    Added result syncing

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const fs = require('fs')
+const path = require('path')
+const bodyParser = require('body-parser');
 // const randomWords = require("./randomWords.json")
 const commonWords = require("./commonWords.json")
 const leaderboard = require("./leaderboard.json")
-
 
 
 
@@ -21,7 +23,7 @@ function generatePrompt()
 {
     var len = commonWords.length
     var outputString = ""
-    for(var i = 0; i < 100; i++)
+    for(var i = 0; i < 10; i++)
     {
         var index = getRandomIntInclusive(0, len - 1)
         if(i != 0)
@@ -34,6 +36,10 @@ function generatePrompt()
 
 app.use(express.static('public'))
 
+//POST to leaderboard won't work without this for some reason
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/new-string', function(req, res, next){
   output = {prompt: generatePrompt()}
   res.status(200).send(output)
@@ -44,6 +50,20 @@ app.get('/leaderboard', function(req, res){
     res.send('Leaderboard')
 })
 
+app.post('/leaderboard', (req, res) => {
+  try {
+    var newEntry = req.body
+    var leaderboardData = getLeaderboardData()
+    leaderboardData.push(newEntry)
+    saveLeaderboardData(leaderboardData);
+    console.log('leaderboard updated successfully')
+    res.status(200).json({ success: true })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+})
+
 app.get('/', function(req, res) {
     //This will get converted to the main page handlebars when its configured
     res.send('Hello, world')
@@ -52,3 +72,28 @@ app.get('/', function(req, res) {
 app.listen(port, function() {
   console.log(`Example app listening on port ${port}`)
 })
+
+var leaderboardFilePath = path.join(__dirname, 'leaderboard.json')
+
+function getLeaderboardData() {
+  try {
+    var data = fs.readFileSync(leaderboardFilePath, 'utf8')
+
+    if (!data.trim()) {
+      return []
+    }
+
+    return JSON.parse(data)
+  } catch (error) {
+    console.error(error)
+    return []
+  }
+}
+
+function saveLeaderboardData(data) {
+  try {
+    fs.writeFileSync(leaderboardFilePath, JSON.stringify(data, null, 2), 'utf8')
+  } catch (error) {
+    console.error(error)
+  }
+}
